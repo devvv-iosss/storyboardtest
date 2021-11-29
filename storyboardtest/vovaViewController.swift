af180c63da3d7061f81055d7251b7e5852ae8896
devvv-iosss <81225988+devvv-iosss@users.noreply.github.com>
2021-11-29 19:33:14 +0300

    redVC

---
+++ storyboardtest/vovaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  vovaViewController.swift
+//  storyboardtest
+//
+//  Created by Anna on 29.11.2021.
+//
+
+import UIKit
+
+class vovaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
